7c9664f11383050499e22e6e229daec4207ed05c
dlatjgus0612 <dlatjgus@gmail.com>
2023-05-19 02:19:29 +0900

    0519

---
+++ 8.dynamicprogramming/개미전사.py
@@ -0,0 +1,11 @@
+n = int(input())
+k = list(map(int, input().split()))
+#DP테이블 초기화
+d = [0]*100 
+#다이나믹 (bottomup)
+d[0] = k[0]
+d[1] = max(k[0], k[1])
+for i in range(2,n):
+    d[i]= max(d[i-1], d[i-2]+k[i])
+
+print(d[n-1])
