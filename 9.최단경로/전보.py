00515ee7847b6e5ca016cbf1a3e9af78555f47d2
ImSH <dlatjgus0612@gmail.com>
2023-07-21 16:30:09 +0900

    0721

---
+++ 9.최단경로/전보.py
@@ -0,0 +1,22 @@
+# 도시개수, 통로개수, start
+n, m, c = map(int, input().split())
+
+# xyz 입력 받기
+xyz = [[map(int, input().split())] for _ in range(m)]
+
+# inf
+INF = int(1e9)
+
+# 결괏값 초기화 테이블 만들기
+d = [INF] * (n+1)
+
+# dijkstra
+def dijkstra(start):
+    xyz[c][0] =
+# startnode = 0
+# 점화식  d[a][b] = min()
+# print (len(y), max(z))
+
+
+
+dijkstra(c)
--- 9.최단경로/플로이드워셜.py
+++ 9.최단경로/플로이드워셜.py
@@ -15,3 +15,18 @@ for _ in range(m):
     # a에서 b가는 비용은 c
     a, b, c = map(int, input().split())
     graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 진행
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# print
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] ==INF :
+            print("INFINITY", end = ' ')
+        else:
+            print(graph[a][b], end = ' ')
+    print()
